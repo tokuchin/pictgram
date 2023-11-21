6e7d3d49481d84ccea9059386a2df9718b0034d4
junya <tokuchin_0918@yahoo.co.jp>
2023-11-21 07:11:29 +0000

    バリデーションを追加 「Rails 04 User新規登録に潜む問題を解決していく」の課題

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ApplicationRecord
     validates :name, presence: true
+    validates :name, length: { maximum: 15 }
+    
     validates :email, presence: true
+    validates :password, length: { minimum: 6,  maximum: 32 }
+    validates :email, format: { with:/\A.+@.+[.].+\z/i  }
+    VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+    validates :password, format: { with: VALID_PASSWORD_REGEX }
     
     has_secure_password
+
 end
