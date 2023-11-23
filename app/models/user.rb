55d20775cf0b281c261d7b706f73b6d583799fa6
junya <tokuchin_0918@yahoo.co.jp>
2023-11-23 06:55:17 +0000

    nameとemailのバリデーションを一行にまとめた

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,8 @@
 class User < ApplicationRecord
-    validates :name, presence: true
-    validates :name, length: { maximum: 15 }
-    
-    validates :email, presence: true
+    validates :name, presence: true, length: { maximum: 15 }
+    validates :email, presence: true, format: { with:/\A.+@.+[.].+\z/i  }
+
     validates :password, length: { minimum: 6,  maximum: 32 }
-    validates :email, format: { with:/\A.+@.+[.].+\z/i  }
     VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
     validates :password, format: { with: VALID_PASSWORD_REGEX }
     
