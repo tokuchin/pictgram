fd76e1b91bff61a0dc4d557ff4583004804563a8
junya <tokuchin_0918@yahoo.co.jp>
2023-11-24 07:40:00 +0000

    「Rails 06 写真投稿機能の作成_2」の課題部分を追記

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -35,13 +35,18 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Add an allowlist of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_allowlist
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_allowlist
+    %w(jpg jpeg png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
   # def filename
   #   "something.jpg" if original_filename
   # end
+  
+  def size_range
+    0..10.megabytes
+  end
+  
 end
