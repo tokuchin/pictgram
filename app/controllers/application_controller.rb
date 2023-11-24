26d779cc0186346c38d1953fe6d166b66e50201c
junya <tokuchin_0918@yahoo.co.jp>
2023-11-24 04:37:26 +0000

    「Rails 05 ログイン機能の実装」の課題部分を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,13 @@ class ApplicationController < ActionController::Base
   add_flash_types :success, :info, :warning, :danger
   
   def current_user
-    @current_user ||= User.find.by(id: session[:user_id])
+    @current_user ||= User.find_by(id: session[:user_id])
   end
   
   def logged_in?
     !current_user.nil?
   end
+  
+  helper_method :current_user, :logged_in?
   
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,8 @@ class SessionsController < ApplicationController
   end
   
   def create
-    user = User.find_by(email: params[:session][:email])
-    if user && user.authenticate(params[:session][:password])
+    user = User.find_by(email: session_params[:session][:email])
+    if user && user.authenticate(session_params[:session][:password])
       log_in user
       redirect_to root_path, success: 'ログインに成功しました'
     else
@@ -27,4 +27,9 @@ class SessionsController < ApplicationController
     session.delete(:user_id)
     @current_user = nil
   end
+
+  def session_params
+    params.require(:session).permit(:email, :password, :password_confirmation)
+  end  
+
 end
