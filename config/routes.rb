180b92c0df9601d11d018652f1cf4364d22c008b
junya <tokuchin_0918@yahoo.co.jp>
2023-11-27 03:28:16 +0000

    「Rails 07 「いいね」機能の作成」の課題の前まで

--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,7 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+  
+  get 'favorites/index'
+  post '/favorites', to: 'favorites#create'
 end
